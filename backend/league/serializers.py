ccbb22e035aacacc4222a13f6bc1b51e953926c5
EddieSbock <a.lamedica1@studenti.poliba.it>
2025-12-13 18:59:28 +0100

    aggiunta sessione

--- backend/league/serializers.py
+++ backend/league/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import House, Profile, Task, SubTask, Assignment
+from .models import House, Profile, Task, SubTask, GameSession, Assignment
 from django.contrib.auth.models import User
 
 
@@ -20,13 +20,30 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+        
+class GameSessionSerializer(serializers.ModelSerializer):
+
+    tasks = TaskSerializer(many=True, read_only=True)
+    
+    duration_hours = serializers.ReadOnlyField()
+
+    class Meta:
+        model = GameSession
+        fields = '__all__'
 
 class HouseSerializer(serializers.ModelSerializer):
     admin_details = UserSerializer(source='admin', read_only=True)# per i dettagli dell'admin
     
+    active_sessions = serializers.SerializerMethodField()
+    
     class Meta:
         model = House
         fields = '__all__'
+        
+    def get_active_sessions(self, obj):
+    
+        sessions = obj.sessions.filter(is_active=True)
+        return GameSessionSerializer(sessions, many=True).data
 
 class ProfileSerializer(serializers.ModelSerializer):
     user_details = UserSerializer(source='user', read_only=True)
--- backend/league/views.py
+++ backend/league/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import House, Profile, Task
-from .serializers import HouseSerializer, ProfileSerializer, TaskSerializer
+from .models import House, Profile, Task, GameSession
+from .serializers import HouseSerializer, ProfileSerializer, TaskSerializer, GameSessionSerializer
 
 class HouseViewSet(viewsets.ModelViewSet):
     queryset = House.objects.all()
@@ -13,3 +13,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    
+class GameSessionViewSet(viewsets.ModelViewSet):
+    queryset = GameSession.objects.all()
+    serializer_class = GameSessionSerializer
