98cae6471aefde82c20de11066fa3530df906e74
EddieSbock <a.lamedica1@studenti.poliba.it>
2025-12-13 19:03:52 +0100

    aggiunta finale game session backend

--- backend/league/admin.py
+++ backend/league/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import House, Profile, Task, Assignment, SubTask
+from .models import House, Profile, Task, Assignment, SubTask, GameSession
 
 admin.site.register(House)
 admin.site.register(Profile)
 admin.site.register(Task)
 admin.site.register(Assignment)
 admin.site.register(SubTask)
+admin.site.register(GameSession)
--- backend/league/urls.py
+++ backend/league/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HouseViewSet, ProfileViewSet, TaskViewSet
+from .views import HouseViewSet, ProfileViewSet, TaskViewSet, GameSessionViewSet
 
 router = DefaultRouter() #per crare automaticamente gli url delle altre pagine
 router.register(r'houses', HouseViewSet)
 router.register(r'profiles', ProfileViewSet)
 router.register(r'tasks', TaskViewSet)
+router.register(r'sessions', GameSessionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
