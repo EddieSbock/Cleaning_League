8b0a69b3f53c951b54fb6641353d69e9c7e9efbd
Nick <n.digiuseppe@studenti.poliba.it>
2025-12-15 15:58:05 +0100

    dinamiche di gioco

---
+++ frontend/src/services/game.js
@@ -0,0 +1,39 @@
+import api from './api';
+
+const gameService = {
+  //contenitore delle task
+  getSessions: async () => {
+    const response = await api.get('sessions/');
+    return response.data;
+  },
+
+  //lista delle task
+  getTasks: async () => {
+    const response = await api.get('tasks/');
+    return response.data;
+  },
+  
+  
+  grabTask: async (taskId) => {
+    const response = await api.post(`tasks/${taskId}/grab/`);
+    return response.data;
+  },
+
+
+  completeAssignment: async (assignmentId) => {
+    const response = await api.post(`assignments/${assignmentId}/complete/`);
+    return response.data; //restituisce i punti guadagnati
+  },
+
+
+  submitRating: async (assignmentId, stars, comment) => {
+    const response = await api.post('ratings/', {
+      assignment: assignmentId,
+      stars: stars, // Numero da 1 a 5
+      comment: comment
+    });
+    return response.data;
+  }
+};
+
+export default gameService;
